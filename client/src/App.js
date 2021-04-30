1a1f97c1b9f39ec874770f5394c030d7e589d7bd
Pjeferson <paulojeferson34@gmail.com>
2021-04-30 11:41:08 -0300

    Define development Dockerfiles; Multi-Containerizing with docker-compose

--- client/src/App.js
+++ client/src/App.js
@@ -7,35 +7,33 @@ import OtherPage from './OtherPage';
 import './App.css';
 import logo from './logo.svg';
 
-export default App = () => {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
 
-        <Link to='/'>Home</Link>
-        <Link to='/otherpage'>Other Page</Link>
-      </header>
+          <Link className='App-link' to='/'>Home</Link>
+          <Link className='App-link' to='/otherpage'>Other Page</Link>
+        </header>
 
-      <div>
-        <Router>
-          <Route exact path='/' component={Fib} />
-          <Route path='/otherpage' component={OtherPage} />
-        </Router>
+        <Route exact path='/' component={Fib} />
+        <Route path='/otherpage' component={OtherPage} />
       </div>
-    </div>
+    </Router>
   );
 }
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default OtherPage = () => {
+export default function OtherPage() {
   return (
     <div>
       Some other page \0v0/
